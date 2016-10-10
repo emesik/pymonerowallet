77138300878c71d36c3138d1ed01a3138a37954e
Carl Chenet <chaica@ohmytux.com>
2016-10-10 10:43:07 +0200

    add docstring in exceptions module

--- monerowallet/exceptions/__init__.py
+++ monerowallet/exceptions/__init__.py
@@ -1,8 +1,25 @@
+"""
+    The ``exceptions`` module
+    =============================
+ 
+    Exceptions raised by PyMoneroWallet.
+ 
+"""
+
 class Error(Exception):
+    '''
+        General PyMoneroWallet exception
+    '''
     pass
 
 class StatusCodeError(Error):
+    '''
+        Returned exception when returned HTTP status code is different from 200
+    '''
     pass
 
 class MethodNotFoundError(Error):
+    '''
+        Returned exception when RCP server not able to understand the request
+    '''
     pass
